cabf738665c2cb9994ca8e63fa179d0167e0f6c3
Brandon Showers <btshowe@sandia.gov>
2020-04-04 16:06:43 +0000

    added api_versions method.

---
+++ api_version/run.ps1
@@ -0,0 +1,16 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+$status = [HttpStatusCode]::OK
+$body = '{"versions":["1.0","1.1","1.2","1.3","1.4","1.5","1.6","1.7","1.8","1.8.1"]}'
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body = $body
+})
--- login/run.ps1
+++ login/run.ps1
@@ -10,16 +10,21 @@ if($api = $Request.Headers."api-token" -eq "2PDoD5iaokKDwGh9uNqt1jpDTNpgshfiOzO6
     # Interact with query parameters or the body of the request.
     if($Request.Method -eq "POST"){
         $status = [HttpStatusCode]::OK
-        $body = 'username: "pureuser"'+"`n"+ 'Response headers: x-auth-token: "52a8163a-803b-475b-9c53-f99f6e7f4c22"'
+        $xAuthToken = "52a8163a-803b-475b-9c53-f99f6e7f4c22"
+        $body = 'username: "pureuser"'+"`n"+ 'Response headers: x-auth-token: "'+$xAuthToken+'"'
     }
 }
 else{
     $status = [HttpStatusCode]::Unauthorized
     $body = "Unauthorized.  Make sure to use the following key and value in your header:`napi-token 2PDoD5iaokKDwGh9uNqt1jpDTNpgshfiOzO643z5ch92Mwycl7veBA=="
+    $xAuthToken = ""
 }
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    headers = @{'content-type'='application\json'}
+    headers = @{
+        'content-type'='application\json'
+        'x-auth-token'= $xAuthToken
+        }
     StatusCode = $status
     Body = $body
 })
